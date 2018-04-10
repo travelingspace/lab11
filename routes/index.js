035cd036c9be056a6cc2753a6a0428e060d1a712
James Houck <jameshouck@live.com>
2018-04-10 17:46:58 -0500

    added fetchpicture route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/*fetch for picture*/
+router.get('/fetchpicture', function(req, res, next){
+
+  if (req.query.picturetype === 'random'){
+    res.send('todo: get random picture');
+  }
+  else{
+    res.send('todo: get today\'s picture');
+  }
+
+});
+
+
+
+//***********************************
 module.exports = router;
