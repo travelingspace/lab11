53a746f355a3a8e484fa6946f28a732653597ae5
James Houck <jameshouck@live.com>
2018-04-10 18:17:31 -0500

    added apod.js and index.js get

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var apodService = require('../services/apod');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next){
+  res.redirect('fetchpicture');
 });
 
 /*fetch for picture*/
 router.get('/fetchpicture', function(req, res, next){
 
-  if (req.query.picturetype === 'random'){
-    res.send('todo: get random picture');
-  }
-  else{
-    res.send('todo: get today\'s picture');
-  }
+  apodService(function(err, apod_data){
+
+      if (err) {
+          res.render('apod_error', {message: err.message, title: 'Error'});
+      }
+      else {
+          res.render('index', {apod: apod_data, title: `APOD for ${apod_data.date}`});
+      }
+  }, req.query.picturetype);
 
 });
 
